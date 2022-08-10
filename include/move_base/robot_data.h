c16480ce5303ec687e0d5abbf49bdb8ef1d96c05
Jarek Karwowski <chromedivizer@gmail.com>
2022-08-11 00:17:53 +0200

    added `RobotData` class

---
+++ include/move_base/robot_data.h
@@ -0,0 +1,113 @@
+#pragma once
+
+#include <geometry_msgs/PoseStamped.h>
+#include <tf2/utils.h>
+
+class RobotData {
+public:
+    RobotData(
+        const geometry_msgs::PoseStamped& robot_pose,
+        const geometry_msgs::PoseStamped& robot_vel,
+        const geometry_msgs::PoseStamped& goal_pose,
+        double obstacle_distance,
+        double exec_time
+    ): RobotData(robot_pose.pose, robot_vel.pose, obstacle_distance, exec_time) {}
+
+    RobotData(
+        const geometry_msgs::Pose& robot_pose,
+        const geometry_msgs::Pose& robot_vel,
+        const geometry_msgs::Pose& goal_pose,
+        double obstacle_distance,
+        double exec_time
+    ):
+        pose_(robot_pose),
+        vel_(robot_vel),
+        goal_(goal_pose),
+        obstacle_distance_(obstacle_distance),
+        local_plan_exec_time_(exec_time)
+    {}
+
+    inline geometry_msgs::Pose getPose() const {
+        return pose_;
+    }
+
+    inline geometry_msgs::Point getPosition() const {
+        return pose_.position;
+    }
+
+    inline geometry_msgs::Quaternion getOrientation() const {
+        return pose_.orientation;
+    }
+
+    inline double getPositionX() const {
+        return pose_.position.x;
+    }
+
+    inline double getPositionY() const {
+        return pose_.position.y;
+    }
+
+    inline double getPositionZ() const {
+        return pose_.position.z;
+    }
+
+    inline double getOrientationYaw() const {
+        return tf2::getYaw(pose_.orientation);
+    }
+
+    inline geometry_msgs::Pose getVelocity() const {
+        return vel_;
+    }
+
+    inline double getVelocityX() const {
+        return vel_.position.x;
+    }
+
+    inline double getVelocityY() const {
+        return vel_.position.y;
+    }
+
+    inline double getVelocityZ() const {
+        return vel_.position.z;
+    }
+
+    inline double getVelocityTheta() const {
+        return tf2::getYaw(vel_.orientation);
+    }
+
+    inline geometry_msgs::Pose getGoal() const {
+        return goal_;
+    }
+
+    inline double getGoalPositionX() const {
+        return goal_.position.x;
+    }
+
+    inline double getGoalPositionY() const {
+        return goal_.position.y;
+    }
+
+    inline double getGoalPositionZ() const {
+        return goal_.position.z;
+    }
+
+    inline double getGoalOrientationYaw() const {
+        return tf2::getYaw(goal_.orientation);
+    }
+
+    inline double getDistToObstacle() const {
+        return obstacle_distance_;
+    }
+
+    /// Returns duration of local plan computations
+    inline double getLocalPlanningTime() const {
+        return local_plan_exec_time_;
+    }
+
+protected:
+    geometry_msgs::Pose pose_;
+    geometry_msgs::Pose vel_;
+    geometry_msgs::Pose goal_;
+    double obstacle_distance_;
+    double local_plan_exec_time_;
+};
--- include/move_base/robot_logger.h
+++ include/move_base/robot_logger.h
@@ -1,7 +1,11 @@
 #pragma once
 
 #include "benchmark_logger.h"
+#include "robot_data.h"
 #include <tf2/utils.h>
+
+// helper functions
+#include <people_msgs_utils/person.h>
 
 class RobotLogger: public BenchmarkLogger {
 public:
@@ -11,32 +15,69 @@ public:
         BenchmarkLogger::start(&log_file_, log_filename_);
     }
 
-    /// Must be called prior to @ref update that takes 1 argument
-    void update(
-        double timestamp,
-        const geometry_msgs::PoseStamped& robot_pose,
-        const geometry_msgs::PoseStamped& robot_vel,
-        double obstacle_distance
-    ) {
+    void update(double timestamp, const RobotData& robot) {
+        if (log_file_ == nullptr) {
+            throw std::runtime_error("File descriptor for RobotLogger was not properly created!");
+        }
+
         fprintf(
             log_file_,
-            "%.3f %.3f %.3f %.3f %.3f %.3f %.3f ",
+            "%9.4f %s\n",
             timestamp,
-            robot_pose.pose.position.x,
-            robot_pose.pose.position.y,
-            tf2::getYaw(robot_pose.pose.orientation),
-            robot_vel.pose.position.x,
-            tf2::getYaw(robot_vel.pose.orientation),
-            obstacle_distance
+            RobotLogger::robotToString(robot).c_str()
         );
-    }
-
-    void update(double exec_time) {
-        fprintf(log_file_, "%.3f\n", exec_time);
     }
 
     void finish() {
         BenchmarkLogger::finish(&log_file_);
+    }
+
+    /// Converts given robot data into string description
+    static std::string robotToString(const RobotData& robot) {
+        char buff[100];
+        sprintf(
+            buff,
+            "%9.4f %9.4f %9.4f %9.4f %9.4f %9.4f %9.4f",
+            robot.getPositionX(),
+            robot.getPositionY(),
+            robot.getOrientationYaw(),
+            robot.getVelocityX(),
+            /* robot.getVelocityY(), */
+            robot.getVelocityTheta(),
+            robot.getDistToObstacle(),
+            robot.getLocalPlanningTime()
+        );
+        return std::string(buff);
+    }
+
+    /// Converts given @ref str string description into robot data
+    static RobotData robotFromString(const std::string& str) {
+        auto vals = people_msgs_utils::Person::parseString<double>(str, " ");
+        assert(vals.size() == 7);
+
+        geometry_msgs::Pose pose;
+        pose.position.x = vals.at(0);
+        pose.position.y = vals.at(1);
+        tf2::Quaternion quat;
+        quat.setRPY(0.0, 0.0, vals.at(2));
+        pose.orientation.x = quat.getX();
+        pose.orientation.y = quat.getY();
+        pose.orientation.z = quat.getZ();
+        pose.orientation.w = quat.getW();
+
+        geometry_msgs::Pose vel;
+        vel.position.x = vals.at(3);
+        // vel.position.y = ;
+        quat.setRPY(0.0, 0.0, vals.at(4));
+        vel.orientation.x = quat.getX();
+        vel.orientation.y = quat.getY();
+        vel.orientation.z = quat.getZ();
+        vel.orientation.w = quat.getW();
+
+        double obst_dist = vals.at(5);
+        double exec_time = vals.at(6);
+
+        return RobotData(pose, vel, obst_dist, exec_time);
     }
 
 protected:
