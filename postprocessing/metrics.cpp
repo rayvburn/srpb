7a17d91c83f148543fa9b3d9e6f8e814ace4e4c5
Jarek Karwowski <chromedivizer@gmail.com>
2022-08-21 12:06:09 +0200

    `postprocessing/metrics` - computations based on Gaussians wrapped into `calculateGaussianStatistics` function

--- postprocessing/metrics.cpp
+++ postprocessing/metrics.cpp
@@ -206,6 +206,78 @@ double calculateGaussian(
 }
 
 /**
+ * @brief Computes min, max, normalized metrics related to gaussian statistics and counts number of space violations
+ *
+ * @param timed_gaussians vector of pairs with, first, timestamp, and second, values of gaussians
+ * @param space_violation_threshold Gaussian values bigger than that will be considered as violation of personal space
+ * @param max_method set to true (default) to use max element from Gaussians to normalize metrics;
+ * false means averaging over all Gaussian occurrences in a current time step
+ *
+ * @return std::tuple<double, double, double, unsigned int> tuple with scores: min, max and normalized to execution
+ * time and number of personal space violations (according to @ref personal_space_threshold)
+ */
+std::tuple<double, double, double, unsigned int> calculateGaussianStatistics(
+  std::vector<std::pair<double, std::vector<double>>> timed_gaussians,
+  double space_violation_threshold,
+  bool max_method = true
+) {
+  // find actual duration
+  double duration = 0.0;
+  for (const auto& tg: timed_gaussians) {
+    duration += tg.first;
+  }
+
+  // find number of personal space / f-formation space violations
+  unsigned int space_violations = 0;
+
+  // find gaussians and recompute according to recognized people (max/sum)
+  double metrics = 0.0;
+  double min_elem = std::numeric_limits<double>::max();
+  double max_elem = std::numeric_limits<double>::min();
+
+  // rollout gaussians and compute score (metrics)
+  for (const auto& tg: timed_gaussians) { // tg in fact might be const but ac
+    double dt = tg.first;
+    if (tg.second.empty()) {
+      std::cout << "Gaussian(s) is empty for at least 1 sample. Metrics will be 0.0" << std::endl;
+      return std::make_tuple(0.0, 0.0, 0.0, 0);
+    }
+
+    space_violations += std::count_if(
+      tg.second.cbegin(),
+      tg.second.cend(),
+      [&](double g) {
+        return g > space_violation_threshold;
+      }
+    );
+
+    // overall min and max computation
+    double local_min_elem = *std::min_element(tg.second.cbegin(), tg.second.cend());
+    if (local_min_elem < min_elem) {
+      min_elem = local_min_elem;
+    }
+
+    double local_max_elem = *std::max_element(tg.second.cbegin(), tg.second.cend());
+    if (local_max_elem > max_elem) {
+      max_elem = local_max_elem;
+    }
+
+    // check for selected method of normalization
+    double metrics_elem = 0.0;
+    if (max_method) {
+      // max method used here for normalization
+      metrics_elem = local_max_elem;
+    } else {
+      // average used for normalization
+      metrics_elem = std::accumulate(tg.second.cbegin(), tg.second.cend(), 0.0) / static_cast<double>(tg.second.size());
+    }
+    metrics += (local_max_elem * (dt / duration));
+  }
+
+  return std::make_tuple(min_elem, max_elem, metrics, space_violations);
+}
+
+/**
  * @brief Computes people personal space intrusion score
  *
  * People have their constrained area attached to the body projection. Once robot moves around people it may come
@@ -296,60 +368,7 @@ std::tuple<double, double, double, unsigned int> computePersonalSpaceIntrusion(
     } // iterating over people log entries
   } // iteration over robot log entries
 
-  // find actual duration
-  double duration = 0.0;
-  for (const auto& tg: timed_gaussians) {
-    duration += tg.first;
-  }
-
-  // find number of personal space violations
-  unsigned int ps_violations = 0;
-
-  // find gaussians and recompute according to recognized people (max/sum)
-  double metrics = 0.0;
-  double min_elem = std::numeric_limits<double>::max();
-  double max_elem = std::numeric_limits<double>::min();
-
-  // rollout gaussians and compute score (metrics)
-  for (const auto& tg: timed_gaussians) { // tg in fact might be const but ac
-    double dt = tg.first;
-    if (tg.second.empty()) {
-      std::cout << "Gaussian(s) is empty for at least 1 sample. Personal space intrusion metrics will be 0.0" << std::endl;
-      return std::make_tuple(0.0, 0.0, 0.0, 0);
-    }
-
-    ps_violations += std::count_if(
-      tg.second.cbegin(),
-      tg.second.cend(),
-      [&](double g) {
-        return g > personal_space_threshold;
-      }
-    );
-
-    // overall min and max computation
-    double local_min_elem = *std::min_element(tg.second.cbegin(), tg.second.cend());
-    if (local_min_elem < min_elem) {
-      min_elem = local_min_elem;
-    }
-
-    double local_max_elem = *std::max_element(tg.second.cbegin(), tg.second.cend());
-    if (local_max_elem > max_elem) {
-      max_elem = local_max_elem;
-    }
-
-    // check for selected method of normalization
-    double metrics_elem = 0.0;
-    if (max_method) {
-      // max method used here for normalization
-      metrics_elem = local_max_elem;
-    } else {
-      // average used for normalization
-      metrics_elem = std::accumulate(tg.second.cbegin(), tg.second.cend(), 0.0) / static_cast<double>(tg.second.size());
-    }
-    metrics += (local_max_elem * (dt / duration));
-  }
-
-  return std::make_tuple(min_elem, max_elem, metrics, ps_violations);
+  return calculateGaussianStatistics(timed_gaussians, personal_space_threshold, max_method);
 }
 
 // string to pair (first = timestamp, second = logged state)
