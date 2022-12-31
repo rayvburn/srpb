329b1c5146fd1b1102c0318c271348328ead0856
Jarek Karwowski <chromedivizer@gmail.com>
2022-12-31 01:49:18 +0100

    `BenchmarkLogger` (base class) - timestamp added to the log filename

--- include/move_base/benchmark_logger.h
+++ include/move_base/benchmark_logger.h
@@ -2,11 +2,32 @@
 
 #include <ros/ros.h>
 
+#include <ctime>
+#include <iomanip>
+#include <sstream>
+
 class BenchmarkLogger {
 public:
     virtual void init(ros::NodeHandle& nh) {
         //path to save the recorded data
         nh.param("log_filename", log_filename_, std::string("log.txt"));
+
+        auto log_path_base = log_filename_.substr(0, log_filename_.find_last_of(EXTENSION_SEPARATOR));
+        auto log_extension = log_filename_.substr(log_filename_.find_last_of(EXTENSION_SEPARATOR) + 1);
+
+        log_filename_ = log_path_base + "_" + BenchmarkLogger::timeToString() + EXTENSION_SEPARATOR + log_extension;
+    }
+
+    /**
+     * @brief Transforms current timestamp to the formatted string
+     * @url https://stackoverflow.com/a/16358111
+     */
+    static std::string timeToString() {
+        auto time = std::time(nullptr);
+        auto tm = *std::localtime(&time);
+        std::ostringstream oss;
+        oss << std::put_time(&tm, "%Y-%m-%d_%H-%M-%S");
+        return oss.str();
     }
 
 protected:
