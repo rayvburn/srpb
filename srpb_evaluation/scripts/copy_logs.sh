9103e32b4badffe4a5909135ae8ccfd916b807b3
Jarek Karwowski <chromedivizer@gmail.com>
2023-08-10 15:29:03 +0200

    `evaluation` - parameterized scripts for managing and processing logs
    - Aim is to avoid copying each script to log directories. Instead, all logs now take parameters that define where are the logs etc.
    - Deleted `evaluate_newest` as its functionality would be the same as `evaluate_from_dir`'s after parameterization

--- srpb_evaluation/scripts/copy_logs.sh
+++ srpb_evaluation/scripts/copy_logs.sh
@@ -1,24 +1,45 @@
 #!/usr/bin/env bash
 #
-# Copies 3 newest files from this directory to the directory given by argument.
-# It is handy to put that script into the logs directory.
+# Copies 3 newest log files (robot/people/groups) from the one directory to the directory given by argument.
 #
-if [ "$#" -ne 1 ]; then
-    echo "What's the target directory?"
+# Usage looks like:
+#
+#   rosrun srpb_evaluation copy_logs.sh  <path_to_the_main_directory_with_logs>  <path_to_the_target_directory>
+#
+if [ "$#" -lt 1 ] || [ "$#" -gt 2 ]; then
+    echo "Wrong usage. Script args:"
+    echo "  (1) [required] path to the source directory with logs ('*_robot.txt', '*_people.txt', and '*_groups.txt')"
+    echo "  (2) [required] path to the target directory with copied logs; relative paths will be considered as relative to the source directory"
     exit 0
 fi
 
-SCRIPT_DIR=$(realpath $(dirname $0))
-TARGET_DIR="$SCRIPT_DIR/$1"
-LOGS_DIR=$SCRIPT_DIR
-mkdir -p $TARGET_DIR
+logs_dir="$1"
+# trim trailing / (if exists)
+logs_dir="${logs_dir%/}"
+
+target_dir="$2"
+# trim trailing / (if exists)
+target_dir="${target_dir%/}"
+
+if [[ ! $target_dir == /* ]]; then
+    echo "Relative path"
+    target_dir=$logs_dir/$target_dir
+fi
+
+# create a new directory for the copied logs
+mkdir -p $target_dir
 
 # https://stackoverflow.com/a/54910963
-files_to_copy=$(ls -t $LOGS_DIR/log_*.txt | head -3)
-cp $files_to_copy $TARGET_DIR
+newest_robot=$(ls -t $logs_dir/log_*_robot.txt | head -1)
+newest_people=$(ls -t $logs_dir/log_*_people.txt | head -1)
+newest_groups=$(ls -t $logs_dir/log_*_groups.txt | head -1)
+
+# added an escape sequence for newline (file paths are printed in separate lines)
+files_to_copy="$newest_robot"$'\n'"$newest_people"$'\n'"$newest_groups"
+cp $files_to_copy $target_dir
 
 echo "Copied:"
 echo ""
 echo "$files_to_copy"
 echo ""
-echo "to $TARGET_DIR"
+echo "to $target_dir"
